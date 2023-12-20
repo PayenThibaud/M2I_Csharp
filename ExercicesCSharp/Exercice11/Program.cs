1cc059d8826c8e764638cdc33e2c103776ca2a80
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-20 16:04:34 +0100

    feat: ajout exercice11

--- ExercicesCSharp/Exercice11/Program.cs
+++ ExercicesCSharp/Exercice11/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- La nombre est-il divisible par... ? ---");
+Console.Write("Entrez un chiffre/nombre entier : ");
+double nombre = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entrez un chiffre/nombre diviseur : ");
+double diviseur = Convert.ToDouble(Console.ReadLine());
+
+
+if ( nombre % diviseur == 0)
+{
+    Console.WriteLine("Le chiffre/nombre est divisible par " + diviseur);
+}
+else
+{
+    Console.WriteLine("Le chiffre/nombre n'est pas divisible par " + diviseur);
+}
