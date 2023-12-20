b34be770443c2578a7abfb320b2b50873dbc311c
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-20 13:54:02 +0100

    feat: ajout exercice9

--- ExercicesCSharp/Exercice9/Program.cs
+++ ExercicesCSharp/Exercice9/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Calcul des intérêts ---");
+Console.Write("Entrez Capital de départ (en Euros) : ");
+double capital = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entrez le taux d'intérêt (en %) : ");
+double interet = Convert.ToDouble(Console.ReadLine());
+Console.Write("Entrez la durée de l'épargne (en années) : ");
+double duree = Convert.ToDouble(Console.ReadLine());
+
+double montant = Math.Round((capital * Math.Pow((1 + interet / 100), duree) - capital), 2);
+double CapF = montant + capital;
+
+Console.WriteLine("Le montant des intérêts sera de : " + montant + " Euros");
+Console.WriteLine("Le capital final sera de : " + CapF + " Euros");
