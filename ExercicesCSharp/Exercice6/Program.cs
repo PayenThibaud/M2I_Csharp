2c2fbd89f82d3a3ca797794e4e6de64c8acdd960
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-20 12:00:12 +0100

    feat: ajout de l'exercice6

---
+++ ExercicesCSharp/Exercice6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("--- Calcul du périmètre et de l'aire d'un carré ---");
+Console.Write("Entrez la longueur d'un coté du carré (en cm) : ");
+double nombre = Convert.ToDouble(Console.ReadLine());
+
+double perimetre = nombre * 4;
+double aire = nombre * nombre;
+Console.WriteLine("Le périmètre du carré est : " + perimetre + " cm");
+Console.WriteLine("La somme de ces deux nombre est : " + aire + " cm2");
