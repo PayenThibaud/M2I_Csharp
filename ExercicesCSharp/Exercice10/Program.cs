ff18f2f1d02c263937fdf7e3c8b3af7a0cc7ceb0
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-20 16:00:30 +0100

    feat: ajout exercice10

---
+++ ExercicesCSharp/Exercice10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("--- La lettre est-elle une voyelle ? ---");
+Console.Write("Entrez qu'UNE lettre : ");
+string lettre = Console.ReadLine();
+
+if (lettre == "a" || lettre == "e" || lettre == "i" || lettre == "o" || lettre == "y" || lettre == "u" || lettre == "A" || lettre == "E" || lettre == "I" || lettre == "O" || lettre == "Y" || lettre == "U")
+{
+    Console.WriteLine("Cette lettre est une voyelle !");
+}
+else if (lettre.Length >= 2)
+{
+    Console.WriteLine("Qu'\"UNE\" Lettre !"); 
+}
+else 
+{
+    Console.WriteLine("Cette lettre n'est pas une voyelle !");
+}
