c48753bbad3c3863cb4ecf2fab4b8b386d15dc28
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-22 11:19:13 +0100

    feat: ajout exercice24-25

---
+++ ExercicesCSharp/Exercice24/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("--- Les suites chaînées de nombre ---");
+Console.Write("Merci de saisir un nombre : ");
+string nombre = Console.ReadLine();
+int somme = 0;
+string cumule = "";
+
+if (double.TryParse(nombre, out double nb))
+{
+    Console.WriteLine("La suite des nombres est :");
+
+    for (int i = 1; i <= nb; i++)
+    {
+        somme = 0;
+        cumule = "";
+        for (int j = i; j <= nb-1; j++)
+        {
+            somme = somme + j;
+            if (cumule == "")
+            {
+                cumule = cumule + j;
+            }
+            else
+            {
+                cumule = cumule + " + " + j;
+            }
+            if (somme == nb)
+                Console.WriteLine(somme +" = " + cumule);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Données entrées invalides");
+}
+ 
---
+++ ExercicesCSharp/Exercice25/Program.cs
@@ -0,0 +1,72 @@
+﻿double bon = 0;
+double nul = 20;
+double moyenne = 0;
+int error = 0;
+
+Console.WriteLine("--- Gestion des notes ---");
+Console.WriteLine("");
+Console.Write("combien de note à saisir ? ");
+string nbnote = Console.ReadLine();
+Console.WriteLine("");
+if (double.TryParse(nbnote, out double nbNote))
+{
+    for (int i = 1; i <= nbNote; i++)
+    {
+        Console.Write($"\t- Merci de saisir la note {i} (sur /20) : ");
+        string nb = Console.ReadLine();
+
+        if (double.TryParse(nb, out double nombre))
+        {
+            if (nombre <= 20 && nombre >= 0)
+            {
+
+                if (bon < nombre)
+                {
+                    bon = nombre;
+                }
+
+                if (nul > nombre)
+                {
+                    nul = nombre;
+                }
+
+                moyenne = moyenne + nombre;
+
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Données entrées invalides");
+                error++;
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Données entrées invalides");
+            error++;
+            break;
+        }
+    }
+    moyenne = Math.Round(moyenne / nbNote, 2);
+
+    if (error == 1)
+    {
+        Console.WriteLine("Vous avez mis une note impossible !");
+    }
+    else
+    {
+        Console.WriteLine("");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("La meilleure note est " + bon);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("La moins bonne note est " + nul); 
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine("La moyenne des notes est " + moyenne); 
+    }
+}
+else
+{
+    Console.WriteLine("Données entrées invalides");
+}
