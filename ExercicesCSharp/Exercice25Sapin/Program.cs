7b0a47edf804735ec85e2891b5de5fad059d729c
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-24 18:19:25 +0100

    feat: ajout du Sapin

---
+++ ExercicesCSharp/Exercice25Sapin/Program.cs
@@ -0,0 +1,62 @@
+﻿//// Affichage en tableau dynamique
+
+int largeurTables = 1;
+
+Console.WriteLine("Combien de branche ?");
+int nbLigne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Hauteur du tronc ?");
+int nbTronc = Convert.ToInt32(Console.ReadLine());
+
+// En arc en ciel
+
+List<ConsoleColor> colors = new List<ConsoleColor>()
+{
+    ConsoleColor.Red,
+    ConsoleColor.Magenta,
+    ConsoleColor.Blue,
+    ConsoleColor.Cyan,
+    ConsoleColor.Green,
+    ConsoleColor.Yellow
+};
+
+
+Console.ForegroundColor = ConsoleColor.White;
+
+for (int i = 1; i <= nbLigne; i++)
+{
+    if (i == 1)
+    {
+        largeurTables = 1;
+    }
+    else
+    {
+        largeurTables += 2;
+    }
+    string espace = string.Concat(Enumerable.Repeat(" ", nbLigne - i + 2));
+    Console.ForegroundColor = colors[i % colors.Count];
+    Console.Write(espace);
+    for (int j = 1; j <= largeurTables; j++)
+    {
+        Console.ForegroundColor = colors[(i + j) % colors.Count];
+        if (i == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("A");
+        }
+        else
+        {
+            Console.Write("*");
+        }
+    }
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+}
+
+for (int i = 1; i <= nbTronc; i++)
+{
+    string espace = string.Concat(Enumerable.Repeat(" ", nbLigne ));
+    Console.WriteLine(espace + "| |");
+}
+
+Console.ForegroundColor = ConsoleColor.White;
---
+++ ExercicesCSharp/Exercice25bis/Program.cs
@@ -0,0 +1,39 @@
+﻿double bon = 0;
+double nul = 20;
+double moyenne = 0;
+
+Console.WriteLine("--- Gestion des notes ---");
+Console.WriteLine("");
+Console.Write("combien de note à saisir ? ");
+double nombreDeNote;
+while (!double.TryParse(Console.ReadLine(), out nombreDeNote))
+    Console.WriteLine("Saisie invalide ! Recommence");
+
+for (int i = 1; i <= nombreDeNote; i++)
+{
+    Console.Write($"\t- Merci de saisir la note {i} (sur /20) : ");
+    double nombre;
+    while (!double.TryParse(Console.ReadLine(), out nombre) || nombre > 20 || nombre < 0)
+        Console.Write($"Saisie invalide ! Recommence \n \t- Merci de saisir la note {i} (sur /20) : ");
+
+    if (bon < nombre)
+        {
+            bon = nombre;
+        }
+
+        if (nul > nombre)
+        {
+            nul = nombre;
+        }
+
+        moyenne = moyenne + nombre;
+}
+moyenne = moyenne / nombreDeNote;
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("La meilleure note est " + bon);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("La moins bonne note est " + nul);
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("La moyenne des notes est " + moyenne);
