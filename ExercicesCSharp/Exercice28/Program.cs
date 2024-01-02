9b039795ce077d3d9aa30f38b100329c65eaa987
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-01-02 11:01:34 +0100

    ajout exercice 28 et 30

---
+++ ExercicesCSharp/Exercice28/Program.cs
@@ -0,0 +1,42 @@
+﻿Random aleatoire = new Random();
+int nbMystere = aleatoire.Next(1, 51);
+int a = 1;
+int nbTentative = 0;
+
+Console.WriteLine("--- Trouver le nombre mystère ---");
+Console.WriteLine("");
+
+int i = 0;
+while (i >= 0)
+{
+    Console.Write("Veuillez saisir un nombre : ? ");
+    Console.WriteLine("");
+
+    while (!int.TryParse(Console.ReadLine(), out a) || a > 50 || a < 0)
+        Console.WriteLine("Saisie invalide ! Recommence");
+
+    nbTentative++;
+
+    if (a > nbMystere)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Le nombre mystère est plus petit ");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    if (a < nbMystere)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Le nombre mystère est plus grand ");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    if (a == nbMystere)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Bravo !!! Vous avez trouvé le nombre mystère ! ");
+        Console.WriteLine("Vous avez trouvé en " + nbTentative + " coups.");
+        Console.ForegroundColor = ConsoleColor.White;
+        break;
+    }
+}
---
+++ ExercicesCSharp/Exercice30/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("--- Question à choix multiple ---");
+Console.WriteLine("");
+Console.WriteLine("Quelle est l'instruction qui permet de sortir d'une boucle en C# ? ");
+Console.WriteLine("\t a) quit");
+Console.WriteLine("\t b) continue");
+Console.WriteLine("\t c) exit");
+Console.WriteLine("\t d) break");
+Console.WriteLine("");
+
+int i = 0;
+while (i >= 0)
+{
+    Console.Write("Entrez votre réponse: ? ");
+    Console.WriteLine("");
+
+    string rep = Console.ReadLine().ToLower();
+    while (rep != "a" && rep != "b" && rep != "c" && rep != "d")
+    {
+        Console.WriteLine("Saisie invalide ! Recommence");
+        rep = Console.ReadLine().ToLower();
+    }
+
+    if (rep == "a" || rep == "b" || rep == "c")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Incorrecte ! ");
+        Console.ForegroundColor = ConsoleColor.White;
+        while (rep != "Oui" && rep != "Non")
+        {
+            Console.Write("Un nouvel essai ? Oui / Non : ");
+            rep = Console.ReadLine();
+        }
+        if (rep == "Non")
+        {
+            break;
+        }
+    }
+
+    if (rep == "d")
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Bravo !!! C'est la bonne réponse ");
+        Console.ForegroundColor = ConsoleColor.White;
+        break;
+    }
+}
