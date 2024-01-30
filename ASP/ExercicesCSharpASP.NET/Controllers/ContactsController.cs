e9a6ea097496ab0219f22833da780d71dbf5fab9
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-01-30 12:11:50 +0100

    Ajout classe Contact, method details

--- ASP/ExercicesCSharpASP.NET/Controllers/ContactsController.cs
+++ ASP/ExercicesCSharpASP.NET/Controllers/ContactsController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Channels;
+using ExercicesCSharpASP.NET.Models;
 
 namespace ExercicesCSharpASP.NET.Controllers
 {
     public class ContactsController : Controller
     {
+
+        private List<Contact> contactsList = new List<Contact>
+{
+    new Contact(1, "Thibaud", "Thibaud@gmail.com", "07070707", "Address1", "Carvin", "HDF", "191919", "France"),
+    new Contact(2, "John", "john@example.com", "12345678", "Address2", "City2", "Region2", "987654", "USA"),
+    new Contact(3, "Alice", "alice@example.com", "87654321", "Address3", "City3", "Region3", "456789", "Canada"),
+    new Contact(4, "Bob", "bob@example.com", "55555555", "Address4", "City4", "Region4", "333333", "UK"),
+    new Contact(5, "Eva", "eva@example.com", "99999999", "Address5", "City5", "Region5", "111111", "Australia")
+};
         public IActionResult Index()
         {
 
@@ -28,9 +38,11 @@ namespace ExercicesCSharpASP.NET.Controllers
 
             return View(contacts);
         }
-            public IActionResult Details(int id)
+        public IActionResult Details(int id)
         {
-            return View();
+            Contact contact = contactsList.FirstOrDefault(c => c.Id == id);
+
+            return View(contact);
         }
 
         public IActionResult Add()
---
+++ ASP/ExercicesCSharpASP.NET/Models/Contact.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExercicesCSharpASP.NET.Models
+{
+    public class Contact
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _phoneNumber;
+        private string _address;
+        private string _city;
+        private string _region;
+        private string _postalCode;
+        private string _country;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public string Address { get => _address; set => _address = value; }
+        public string City { get => _city; set => _city = value; }
+        public string Region { get => _region; set => _region = value; }
+        public string PostalCode { get => _postalCode; set => _postalCode = value; }
+        public string Country { get => _country; set => _country = value; }
+
+        public Contact(int id,string name, string email, string phoneNumber, string address, string city, string region, string postalCode, string country)
+        {
+            _id = id;
+            _name = name;
+            _email = email;
+            _phoneNumber = phoneNumber;
+            _address = address;
+            _city = city;
+            _region = region;
+            _postalCode = postalCode;
+            _country = country;
+        }
+    }
+}
+     
