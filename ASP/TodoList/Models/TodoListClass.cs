790db2a1f02a48a5311fa228009e881eb68213ee
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-02-01 15:35:15 +0100

    Init TodoListClass, Class Todolist + ApplicationDbContext

---
+++ ASP/TodoList/Models/TodoListClass.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class TodoListClass
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Titre de la tâche")]
+        public string Titre { get; set; }
+
+        [Display(Name = "Description de la tâche")]
+        public string Description { get; set; }
+    }
+}
