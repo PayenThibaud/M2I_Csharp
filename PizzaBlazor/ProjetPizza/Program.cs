4ec73618961ffae3e837fac0cfe466ba77af9575
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-02-16 10:46:41 +0100

    Pizza front

--- PizzaBlazor/ProjetPizza/Program.cs
+++ PizzaBlazor/ProjetPizza/Program.cs
@@ -1,10 +1,13 @@
-using DemoBlazer;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ProjetPizza;
+using ProjetPizza.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<IPizzaService, PizzaFakeDbService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
