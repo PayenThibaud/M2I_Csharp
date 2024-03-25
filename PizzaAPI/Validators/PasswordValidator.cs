79ea53f6f89f7c61be9843d6cf5fbde15856b045
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-03-25 16:26:11 +0100

    modifacation password V.0.3

--- PizzaAPI/Validators/PasswordValidator.cs
+++ PizzaAPI/Validators/PasswordValidator.cs
@@ -19,13 +19,13 @@ namespace PizzCore.Validators
                 var hasNumber = new Regex(@"[0-9]{4}");
                 var hasUpperLetters = new Regex(@"[A-Z]{1}");
                 var hasEnoughChars = new Regex(@".{8,15}");
-                var hasLowerLetters = new Regex(@"[a-z]{2}");
+                var hasLowerLetters = new Regex(@"[a-z]{1}");
                 var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]{2}");
 
                 if (!hasEnoughChars.IsMatch(input))
                     ErrorMessage += "Password should not be less than 8 characters and greater than 15 characters. ";
                 if (!hasLowerLetters.IsMatch(input))
-                    ErrorMessage += "Password should contains at least two lower case letters. ";
+                    ErrorMessage += "Password should contains at least one lower case letters. ";
                 if (!hasUpperLetters.IsMatch(input))
                     ErrorMessage += "Password should contains at least one upper case letters. ";
                 if (!hasNumber.IsMatch(input))
